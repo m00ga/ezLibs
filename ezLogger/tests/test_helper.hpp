934997ad0dc9b371f295ac39740659b4f87855ff
m00ga <sunrineme@gmail.com>
2023-10-05 10:30:33 +0300

    ezLogger: now using ezTest for tests

--- ezLogger/tests/test_helper.hpp
+++ ezLogger/tests/test_helper.hpp
@@ -1,25 +1,3 @@
-#pragma once
-
-#include <cstdio>
-
-#ifndef TEST_LABEL
 #define TEST_LABEL "ezLogger"
-#endif
 
-#define TEST_CASE_(label, err_message, test_func)                              \
-  {                                                                            \
-    if (!test_func()) {                                                        \
-      std::printf("%s: %s assertion error\n", label, err_message);             \
-      return 1;                                                                \
-    }                                                                          \
-  }
-
-#define TEST_ASSERT(assert)                                                    \
-  TEST_CASE_(TEST_LABEL, #assert, [&]() { return assert; })
-
-#define TEST_ASSERTL(label, assert)                                            \
-  TEST_CASE_(label, #assert, [&]() { return assert; })
-
-#define TEST_FUNC(func) TEST_CASE_(TEST_LABEL, #func, func)
-
-#define TEST_FUNCL(label, func) TEST_CASE_(label, #func, func)
+#include <ezTest/ezTest.hpp>
