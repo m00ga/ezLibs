0cee025cb790a1f5c4047be3e0f86e43abedb146
m00ga <sunrineme@gmail.com>
2023-10-05 10:30:00 +0300

    ezTest: initial commit

---
+++ ezTest/include/ezTest/ezTest.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdio>
+
+#ifndef TEST_LABEL
+#define TEST_LABEL "unknown"
+#endif
+
+#define TEST_CASE_(label, err_message, test_func)                              \
+  {                                                                            \
+    if (!test_func()) {                                                        \
+      std::printf("%s: %s assertion error\n", label, err_message);             \
+      return 1;                                                                \
+    }                                                                          \
+  }
+
+#define TEST_ASSERT(assert)                                                    \
+  TEST_CASE_(TEST_LABEL, #assert, [&]() { return assert; })
+
+#define TEST_ASSERTL(label, assert)                                            \
+  TEST_CASE_(label, #assert, [&]() { return assert; })
+
+#define TEST_FUNC(func) TEST_CASE_(TEST_LABEL, #func, func)
+
+#define TEST_FUNCL(label, func) TEST_CASE_(label, #func, func)
